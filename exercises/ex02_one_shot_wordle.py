c504664b503439ebc6a0141ef6a829c4603b5a5e
Lindsay Bean <lcbean@unc.edu>
2022-09-05 14:46:18 -0400

    Final Wordle Changes

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,21 +2,29 @@
 
 __author__ = "730557892"
 
-secret_word: str = ("python")
+# picking a secret word (python in this case) and asking the user to guess a word with the same amount of letters
 
-user_guess: str = input("What is your 6-letter guess? ")
+secret_word: str = ("python")
+letters: int = len(secret_word)
+user_guess: str = input(f"What is your {letters}-letter guess? ")
 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
+# while loop checks to see if the player's guess is the same amount of letters as the secret word and if not asks them to guess again 
+# until they pick a word with the right amount of letters
+
 while len(user_guess) != len(secret_word):
-    letters: int = len(secret_word)
     user_guess = input(f"That was not {letters} letters! Try again: ")
         
 current_letter: int = 0
 result: str = ""
 
+# checks each letter in the guessed word for matching letters in the secret word
+# if the letter is the same at the same index on each word a green box is added, if not, a while loop checks if the current letter
+# being checked in the user's guess is at another index in the secret word, if this is true, a yellow box is added and if not
+# a white box is added. The next letter is then checked for a match as the loop resets
 
 while current_letter < len(secret_word):
     if secret_word[current_letter] == user_guess[current_letter]:
@@ -36,6 +44,8 @@ while current_letter < len(secret_word):
     current_letter = current_letter + 1
 
 print(result)
+
+#the player wins if they guess the word right and lose if they're wrong
 
 if user_guess != secret_word:
     print("Not quite. Play again soon!")
