e2839b59800934e5a937ac0cf094bc0b5666a84d
Lindsay Bean <lcbean@unc.edu>
2022-09-12 17:21:09 -0400

    changes to ex03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -34,9 +34,32 @@ def emojified(user_guess: str, secret_word: str) -> str:
                 emoji_result += WHITE_BOX
         i += 1
     return emoji_result 
-    
 
+def input_guess(expected_length: int) -> str:
+    """Returns the user's guess provided it is the correct length according to the secret word."""
+    letters: int = expected_length
+    user_guess: str = input(f"Enter a {letters} character word: ")
+    while len(user_guess) != expected_length:
+        user_guess = input(f"That was not {letters} chars! Try again: ")
+    return user_guess
 
-            
-        
+def main() -> None: 
+    """The entrypoint of the program and main game."""
+    secret_word: str = "codes"
+    expected_length: int = len(secret_word)
+    i: int = 1
+    win: bool = False
+    while i <= 6 and win == False:
+        print(f"=== Turn {i}/6 ===")
+        user_guess: str = input_guess(expected_length)
+        print(emojified(user_guess, secret_word))
+        if user_guess == secret_word:
+            print(f"You won in {i}/6 turns!")
+            win = True
+        else:
+            i += 1
+    if win == False:
+        print("X/6 - Sorry, try again tomorrow!")
 
+if __name__ == "__main__":
+    main()
