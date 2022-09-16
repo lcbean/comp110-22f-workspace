987d53330846266855ce285266c5198698cf28bc
Lindsay Bean <lcbean@unc.edu>
2022-09-16 16:36:08 -0400

    Finished ex04 with comments

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -9,6 +9,8 @@ def all(numbers: list[int], single_number: int) -> bool:
         return False
     i: int = 0
     all_same: bool = False
+    # Works through each index of a list to see if the number matches, if it doesn't at any point, returns False
+    # If they match at each index, returns True at the end
     while i < len(numbers):
         if single_number == numbers[i]:
             all_same = True
@@ -23,12 +25,15 @@ def max(numbers: list[int]) -> int:
     if len(numbers) == 0:
         raise ValueError("max() arg is an empty List")
     i: int = 1
+    # Highest number starts out as the first in the list
     highest_number: int = numbers[0]
     while i < len(numbers):
+        # If the number at the next index is higher than the current highest number, it becomes the new highest number 
         if numbers[i] > highest_number:
             highest_number = numbers[i]
         else: 
             i += 1
+    # The reigning highest number is returned
     return highest_number
         
 
@@ -36,8 +41,13 @@ def is_equal(list_1: list[int], list_2: list[int]) -> bool:
     """Given two lists of ints, return True if each index is exactly the same."""
     i: int = 0
     not_equal: bool = False
+    # If both lists are empty, they match so return True
     if len(list_1) == 0 and len(list_2) == 0:
         return True
+    # While i is less than the length of both the lists (in case one is greater than the other) 
+    # if the first index of both lists don't match or if their lengths don't match, return False
+    # else the indices must match so the loop repeats until either an index ends up not matching
+    # or they all end up matching
     while i < len(list_1) and i < len(list_2):
         if list_1[i] != list_2[i] or len(list_1) != len(list_2):
             return False 
